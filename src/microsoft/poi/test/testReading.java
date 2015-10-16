0917a9bdaca98bcd170f193dc58aa0b9dd21c942
Tony's laptop <gzink@live.cn>
2015-10-15 23:54:28 -0400

    POI works in Windows, but there are some problems in Linux

---
+++ src/microsoft/poi/test/testReading.java
@@ -0,0 +1,66 @@
+package microsoft.poi.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class testReading {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			FileInputStream file = new FileInputStream(new File("/home/tony/Desktop/test.xls"));
+			
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+			
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			
+			Iterator<Row> rowIterator = sheet.iterator();
+			
+			while(rowIterator.hasNext()){
+				
+				Row row = rowIterator.next();
+				
+				Iterator<Cell> cellIterator = row.cellIterator();
+				
+				while(cellIterator.hasNext()){
+					
+					Cell cell = cellIterator.next();
+					
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_BOOLEAN:
+						System.out.println(cell.getBooleanCellValue());
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.println(cell.getNumericCellValue());
+						break;
+					case Cell.CELL_TYPE_STRING:
+						System.out.println(cell.getStringCellValue());
+						break;
+					}
+					System.out.println("");
+				}
+				file.close();
+				FileOutputStream out = new FileOutputStream(new File("/home/tony/Desktop/test2.xls"));
+				workbook.write(out);
+				out.close();
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+	
+	}
+	
+}
