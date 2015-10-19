189c2771f3ab29ed9600c38baffb991591ef8b4f
Tony's laptop <gzink@live.cn>
2015-10-18 23:31:56 -0400

    POI transfer to JSON, v1.0 (not satisfy for the front end)

--- src/microsoft/poi/test/testReading.java
+++ src/microsoft/poi/test/testReading.java
@@ -2,62 +2,95 @@ package microsoft.poi.test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.Iterator;
+import java.util.Spliterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 public class testReading {
 
 	public static void main(String[] args) {
-		
+
 		try {
-			
+
 			FileInputStream file = new FileInputStream(new File("/home/tony/Desktop/test.xlsx"));
-			System.out.println(file);
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
-			
+
+			JSONObject json = new JSONObject();
+			System.out.println("hi");
+
 			XSSFSheet sheet = workbook.getSheetAt(0);
-			
-			Iterator<Row> rowIterator = sheet.iterator();
-			while(rowIterator.hasNext()){
-				
-				Row row = rowIterator.next();
-				
-				Iterator<Cell> cellIterator = row.cellIterator();
-				
-				while(cellIterator.hasNext()){
-					
-					Cell cell = cellIterator.next();
-					
+
+			JSONArray jrow = new JSONArray();
+
+			for (Iterator<Row> rowsIT = sheet.rowIterator(); rowsIT.hasNext();) {
+				Row row = rowsIT.next();
+				JSONObject jRow = new JSONObject();
+
+				JSONArray cells = new JSONArray();
+				for (Iterator<Cell> cellsIT = row.cellIterator(); cellsIT.hasNext();) {
+
+					Cell cell = cellsIT.next();
 					switch (cell.getCellType()) {
-					case Cell.CELL_TYPE_BOOLEAN:
-						System.out.println("Hi1");
-						System.out.println(cell.getBooleanCellValue());
-						break;
 					case Cell.CELL_TYPE_NUMERIC:
-						System.out.println(cell.getNumericCellValue());
+						cells.add(cell.getNumericCellValue());
 						break;
 					case Cell.CELL_TYPE_STRING:
-						System.out.println(cell.getStringCellValue());
+						cells.add(cell.getStringCellValue());
 						break;
 					}
-					System.out.println("");
+
 				}
-				file.close();
-				FileOutputStream out = new FileOutputStream(new File("/home/tony/Desktop/test2.xlsx"));
-				workbook.write(out);
-				out.close();
+				System.out.println(cells.toString());
+				jRow.put("cell", cells);
+				jrow.add(jRow);
+
 			}
-			
-			
+
+			json.put("rows", jrow);
+			System.out.println(json.toString());
+
+			// Iterator<Row> rowIterator = sheet.iterator();
+			// while(rowIterator.hasNext()){
+			//
+			// Row row = rowIterator.next();
+			//
+			// Iterator<Cell> cellIterator = row.cellIterator();
+			//
+			// while(cellIterator.hasNext()){
+			//
+			// Cell cell = cellIterator.next();
+			//
+			// switch (cell.getCellType()) {
+			// case Cell.CELL_TYPE_BOOLEAN:
+			// System.out.println(cell.getBooleanCellValue());
+			// break;
+			// case Cell.CELL_TYPE_NUMERIC:
+			// System.out.println(cell.getNumericCellValue());
+			// break;
+			// case Cell.CELL_TYPE_STRING:
+			// System.out.println(cell.getStringCellValue());
+			// break;
+			// }
+			// System.out.println("");
+			// }
+			// file.close();
+			// FileOutputStream out = new FileOutputStream(new
+			// File("/home/tony/Desktop/test2.xlsx"));
+			// workbook.write(out);
+			// out.close();
+			// }
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
